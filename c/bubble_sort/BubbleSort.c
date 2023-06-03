6222a1f9ed63f42d5727eb0d328fc06ff2859819
Hakan Güneş <enghakangunes@gmail.com>
2023-06-04 00:39:38 +0300

    BubbleSort algorithm is developed

--- c/bubble_sort/BubbleSort.c
+++ c/bubble_sort/BubbleSort.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 
+void swap(int* p1, int* p2){
+	int t = *p1;
+	*p1 = *p2;
+	*p2 = t;
+}
+
+void bubbleSort(int* arr, size_t size){
+	for(size_t i=size-1;i>0;i--){
+		for(size_t j=0;j<i;j++){
+			if(arr[j]>arr[j+1]) swap(&arr[j], &arr[j+1]);
+		}
+	}
+}
+
+int main(){
+	int arr[] = {5,3,7,6,1,9,4,8,2};
+	size_t size = sizeof(arr)/sizeof(int);
+	bubbleSort(arr,size);
+	for(size_t i =0;i<size;i++) printf("%d\t",arr[i]);
+	printf("\n");
+	return 0;
+}
+
