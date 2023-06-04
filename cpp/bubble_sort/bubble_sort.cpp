d204224386cde540fac98dad63b7e68ac1d310e5
Hakan Güneş <enghakangunes@gmail.com>
2023-06-04 22:09:34 +0300

    bubble_sort algorithm is developed in cpp by using vector class

--- cpp/bubble_sort/bubble_sort.cpp
+++ cpp/bubble_sort/bubble_sort.cpp
@@ -2,9 +2,17 @@
 #include <vector>
 using namespace std;
 
+void BubbleSort(vector<int> &arr){
+	for(auto i = arr.size()-1;i>0;i--){
+		for(auto j = 0; j < i; j++){
+			if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+		}
+	}
+}
+
 int main(){
 	vector<int> arr={3,6,5,7,1,8,2,9,4};
-//	bubble_sort(&arr);
+	BubbleSort(arr);
 	for(auto i:arr) cout << i << "\t";
 	cout << endl;
 	return 0;
